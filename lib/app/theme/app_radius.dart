c1aa69ebb0fa1711819b3a8efb85bf9189d75021
Gouzou Blé Elie <eliegouzou8@gmail.com>
2026-08-03 11:43:53 +0000

    suppression de tout le continu

--- lib/app/theme/app_radius.dart
+++
@@ -1,9 +0,0 @@
-class AppRadius {
-  const AppRadius._();
-
-  static const double xs = 4;
-  static const double sm = 6;
-  static const double md = 8;
-  static const double lg = 12;
-  static const double xl = 16;
-}
